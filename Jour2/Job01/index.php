c37e29729ae13b970088b53385a90cf8d31c8a86
Lucas259746 <lucas.rozotte@laplateforme.io>
2025-12-11 08:46:38 +0100

    Job2 done

---
+++ Jour2/Job01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Job01</title>
+</head>
+
+<body>
+    <p id="monTexte">Citation.</p>
+    <button id="monBouton">Clique-moi</button>
+
+    <script src="script.js"></script>
+
+</body>
+
+</html>
---
+++ Jour2/Job01/script.js
@@ -0,0 +1,9 @@
+const texte = document.getElementById("monTexte");
+const bouton = document.getElementById("monBouton");
+
+bouton.addEventListener("click", () => {
+    texte.textContent = "La vie a beaucoup plus d’imagination que nous";
+});
+
+
+
